7fb27360db3bf32930ce932f4a7b22b0a3c88c41
Donald Chow <donald7290@gmail.com>
2023-08-26 22:24:01 +0900

    Update so only non-voted restaurant will show up for user

--- app/controllers/vote_sessions_controller.rb
+++ app/controllers/vote_sessions_controller.rb
@@ -2,7 +2,7 @@ class VoteSessionsController < ApplicationController
   before_action :set_vote_session, only: %i[show update]
   def show
     authorize @vote_session
-    @votes = @vote_session.votes.where(user: current_user).shuffle if @vote_session.start
+    @votes = @vote_session.votes.where(user: current_user).where(result: nil).shuffle if @vote_session.start
 
     render json: {
       status: :ok,
