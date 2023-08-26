21178ecbc6e45bcee99aeafb3fc96e43a3555e91
Donald Chow <donald7290@gmail.com>
2023-08-26 16:18:30 +0900

    Confirmed association and amended seeds

--- app/models/vote_session.rb
+++ app/models/vote_session.rb
@@ -1,7 +1,7 @@
 class VoteSession < ApplicationRecord
   belongs_to :group
-  belongs_to :restaurant_list
-  belongs_to :restaurant
+  belongs_to :restaurant_list, optional: true
+  belongs_to :restaurant, optional: true
 
   has_many :group_members, through: :group
   has_many :user, through: :group_members
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
 # Clearning Database
+puts 'Destroying VoteSession'
+VoteSession.destroy_all
+
 puts 'Destroying Restaurants'
 Restaurant.destroy_all
 
-puts 'Destroying Users'
-User.destroy_all
-
 puts 'Destroying Lists'
 RestaurantList.destroy_all
+
+puts 'Destroying groups'
+Group.destroy_all
+
+puts 'Destroying Users'
+User.destroy_all
 
 
 # Creating test users
@@ -22,30 +28,30 @@ puts 'Creating Restaurants'
 Restaurant.create(name: 'Land', category: 'Curry')
 Restaurant.create(name: 'Shokumuso', category: 'Lamb')
 Restaurant.create(name: 'I-rottah', category: 'Pizza')
-Restaurant.create(name: 'Ramen Jiro Meguro', category: 'Ramen')
-Restaurant.create(name: 'Mouko Tanmen Nakamoto Meguro', category: 'Ramen')
-Restaurant.create(name: '目黒インドカレ-Madhuban', category: 'Indian')
-Restaurant.create(name: 'Issunboshi', category: 'Ramen')
-Restaurant.create(name: 'Regionale', category: 'Italian')
-Restaurant.create(name: 'Sukiya', category: 'Beef Don')
-Restaurant.create(name: 'Shapla India', category: 'Indian')
-Restaurant.create(name: 'SKaisen Misakikō Meguro', category: 'Sushi')
-Restaurant.create(name: 'Mos Burger', category: 'Burger')
-Restaurant.create(name: 'Bangkok Oriental Thai Restaurant', category: 'Thai')
-Restaurant.create(name: 'Yousyu Syonin Meguro Honten', category: 'Chinese Ramen')
-Restaurant.create(name: 'Go Bring Slider', category: 'Sliders')
-Restaurant.create(name: 'We Are The Farm', category: 'Salad')
-Restaurant.create(name: 'PIZZA CHECK', category: 'Pizza')
-Restaurant.create(name: 'Kaduya', category: 'Ramen')
-Restaurant.create(name: 'Pizza Giardino Savoy', category: 'Pizza')
-Restaurant.create(name: 'Katsu Midori Meguro', category: 'Sushi')
-Restaurant.create(name: 'Yakiniku Myeongdong', category: 'Korean')
-Restaurant.create(name: 'Tonkatsu Taihou', category: 'Pork Cutlet')
-Restaurant.create(name: 'Takemura', category: 'Ramen')
-Restaurant.create(name: 'Kosuge', category: 'Soba')
-Restaurant.create(name: 'Wagaya', category: 'Korean')
-Restaurant.create(name: 'WAWICH', category: 'Vietnamese')
-Restaurant.create(name: 'Joint', category: 'Burger')
+# Restaurant.create(name: 'Ramen Jiro Meguro', category: 'Ramen')
+# Restaurant.create(name: 'Mouko Tanmen Nakamoto Meguro', category: 'Ramen')
+# Restaurant.create(name: '目黒インドカレ-Madhuban', category: 'Indian')
+# Restaurant.create(name: 'Issunboshi', category: 'Ramen')
+# Restaurant.create(name: 'Regionale', category: 'Italian')
+# Restaurant.create(name: 'Sukiya', category: 'Beef Don')
+# Restaurant.create(name: 'Shapla India', category: 'Indian')
+# Restaurant.create(name: 'SKaisen Misakikō Meguro', category: 'Sushi')
+# Restaurant.create(name: 'Mos Burger', category: 'Burger')
+# Restaurant.create(name: 'Bangkok Oriental Thai Restaurant', category: 'Thai')
+# Restaurant.create(name: 'Yousyu Syonin Meguro Honten', category: 'Chinese Ramen')
+# Restaurant.create(name: 'Go Bring Slider', category: 'Sliders')
+# Restaurant.create(name: 'We Are The Farm', category: 'Salad')
+# Restaurant.create(name: 'PIZZA CHECK', category: 'Pizza')
+# Restaurant.create(name: 'Kaduya', category: 'Ramen')
+# Restaurant.create(name: 'Pizza Giardino Savoy', category: 'Pizza')
+# Restaurant.create(name: 'Katsu Midori Meguro', category: 'Sushi')
+# Restaurant.create(name: 'Yakiniku Myeongdong', category: 'Korean')
+# Restaurant.create(name: 'Tonkatsu Taihou', category: 'Pork Cutlet')
+# Restaurant.create(name: 'Takemura', category: 'Ramen')
+# Restaurant.create(name: 'Kosuge', category: 'Soba')
+# Restaurant.create(name: 'Wagaya', category: 'Korean')
+# Restaurant.create(name: 'WAWICH', category: 'Vietnamese')
+# Restaurant.create(name: 'Joint', category: 'Burger')
 
 puts "Created #{Restaurant.count} restaurants"
 
@@ -60,7 +66,7 @@ puts "Created #{RestaurantList.count} list"
 puts 'Creating list bookmarks'
 
 Restaurant.all.each do |restaurant|
-  ListBookmark.create(restaurant:, restaurant_list: list)
+ListBookmark.create(restaurant:, restaurant_list: list)
 end
 
 puts "Created #{ListBookmark.count} list bookmarks"
@@ -86,3 +92,12 @@ GroupMember.create(group:, user: User.last)
 
 puts "Created #{group.group_members.count} group members for #{group.name}."
 puts "Admin is #{group.group_members.find_by_admin(true).user.name}"
+
+puts 'Creating sessions'
+
+# Create a finished session
+VoteSession.create(group:, name: "Yesterday's Lunch", restaurant_list: list, restaurant: Restaurant.first)
+# create empty vote session
+VoteSession.create(group:, name: "Today's Lunch")
+
+puts "Created #{VoteSession.count} vote session for #{group.name}."
