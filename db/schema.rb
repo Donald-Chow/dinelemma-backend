f0b158bfe721ca5bbbaabd60409549cf334388a3
Donald Chow <donald7290@gmail.com>
2023-08-26 11:48:49 +0900

    Created Restaurant model and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_20_075635) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_022641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
