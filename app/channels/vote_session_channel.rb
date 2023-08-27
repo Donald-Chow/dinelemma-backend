364abea4ce81c68ec3ec56f5fee18978948790eb
Donald Chow <donald7290@gmail.com>
2023-08-27 23:49:26 +0900

    added log message for channel subscription

--- app/channels/vote_session_channel.rb
+++ app/channels/vote_session_channel.rb
@@ -1,6 +1,7 @@
 class VoteSessionChannel < ApplicationCable::Channel
   def subscribed
     stream_from "vote_session_#{params[:id]}"
+    logger.info "#{current_user.name} subscribed to VoteSessionChannel for vote_session_#{vote_session.id}"
   end
 
   def unsubscribed
