373903214da04bc81ecfa8ddf392006ac11622b5
Donald Chow <donald7290@gmail.com>
2023-08-28 21:48:15 +0900

    Fixed pundit in getting current_user

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,5 +1,11 @@
 class CurrentUserController < ApplicationController
+  skip_after_action :verify_policy_scoped
+
   def index
-    render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+    if user_signed_in?
+      render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+    else
+      render json: { message: "User not logged in" }, status: :ok
+    end
   end
 end
